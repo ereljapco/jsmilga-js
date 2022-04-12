0205b6a8f50975c4c45cc4e392b326205a89a399
Erel Ropeta <helloerelita@gmail.com>
2022-04-12 16:18:15 +0800

    feat(variables): Log the values of firstName, last_name, and address in the console

---
+++ 01-variables/app.js
@@ -0,0 +1,10 @@
+let firstName, last_name;
+firstName = "Erel";
+last_name = "Japco";
+
+let address = "main street";
+address = "first street";
+
+console.log(firstName);
+console.log(last_name);
+console.log(address);

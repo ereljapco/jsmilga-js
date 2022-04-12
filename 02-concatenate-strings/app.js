97df39b144d47686e885fef1b473f1fc793611d0
Erel Ropeta <helloerelita@gmail.com>
2022-04-12 17:04:04 +0800

    feat(concatenate-strings): add initial files for concatenate strings challenge

---
+++ 02-concatenate-strings/app.js
@@ -0,0 +1,10 @@
+/*
+## 02 Concatenate Strings
+
+1. create "street" and "country" variables
+2. assign your values
+3. create "fullMailingAddress" variable and assign the result of "street + country"
+4. remember about the space
+5. log "fullMailing Address" in the console
+
+*/

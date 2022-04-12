8de5d032646a17382379d5e67c53305c3b5ec8f3
Erel Ropeta <helloerelita@gmail.com>
2022-04-12 16:31:32 +0800

    comment(variables): add instructions for variables challenge

--- 01-variables/app.js
+++ 01-variables/app.js
@@ -1,3 +1,14 @@
+/*
+## 01 Variables
+
+1. create "firstName" and "last_name" variables
+2. assign your values
+3. create "address" variable and assign "main street" value to it
+4. re-assign address to "first street" later
+5. log all values in the console
+
+*/
+
 let firstName, last_name;
 firstName = "Erel";
 last_name = "Japco";

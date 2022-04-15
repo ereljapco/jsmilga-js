d0bf1d23d1fb5041ce6c67a993431b1ed2bfdacb
Erel Ropeta <helloerelita@gmail.com>
2022-04-16 06:03:46 +0800

    feat(objects): add make, model, year, colors, and hybrid to car

--- 06-objects/app.js
+++ 06-objects/app.js
@@ -1,4 +1,9 @@
 // create car object
+// add make, model, year, colors (array), hybrid (boolean) keys
 const car = {
-  
+  make: 'Toyota',
+  model: 'Corolla',
+  year: '2022',
+  colors: ['White', 'Pearl White', 'Black', 'Red'],
+  hybrid: true
 }

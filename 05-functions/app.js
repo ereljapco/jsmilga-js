271184405699e011fd7470fda9d7f18f38af2362
Erel Ropeta <helloerelita@gmail.com>
2022-04-16 03:15:21 +0800

    feat(functions): create 'order1', 'order2', and 'order3' variables

--- 05-functions/app.js
+++ 05-functions/app.js
@@ -4,3 +4,8 @@ function calculateTotal(subTotal, tax) {
   // return sum of parameters
   return subTotal + tax;
 }
+
+// create 3 variables "order1", "order2", "order3"
+const order1 = null;
+const order2 = null;
+const order3 = null;

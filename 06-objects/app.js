4bf61896afa4d7a488a7848feb40abed691a0215
Erel Ropeta <helloerelita@gmail.com>
2022-04-16 06:05:08 +0800

    feat(objects): add drive and stop methods

--- 06-objects/app.js
+++ 06-objects/app.js
@@ -1,9 +1,16 @@
 // create car object
 // add make, model, year, colors (array), hybrid (boolean) keys
+// add two methods (drive and stop)
 const car = {
   make: 'Toyota',
   model: 'Corolla',
   year: '2022',
   colors: ['White', 'Pearl White', 'Black', 'Red'],
-  hybrid: true
+  hybrid: true,
+  drive() {
+
+  },
+  stop() {
+    
+  }
 }

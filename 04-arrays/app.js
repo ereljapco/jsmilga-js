6c31b942a5883c5d357a87eac5b3d2042c8759dd
Erel Ropeta <helloerelita@gmail.com>
2022-04-16 01:42:53 +0800

    feat(arrays): assign the first item in fruits array to firstFruit

--- 04-arrays/app.js
+++ 04-arrays/app.js
@@ -1,3 +1,6 @@
 // create "fruits" array and store some fruit values
 // setup the last item as number (random)
 let fruits = ['apple', 'banana', 'cherry', 27];
+
+// assign first fruit to the variable
+const firstFruit = fruits[0];

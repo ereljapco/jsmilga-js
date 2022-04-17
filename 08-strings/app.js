e504c99cdffe7ded49c219892a26f12869638435
Erel Ropeta <helloerelita@gmail.com>
2022-04-17 22:30:46 +0800

    fix(strings): add a space between firstName and lastName

--- 08-strings/app.js
+++ 08-strings/app.js
@@ -2,7 +2,7 @@
 // accept two parameters "firstName" and "lastName"
 function fullName(firstName, lastName) {
   // add "firstName" and "lastName" (concat) and return result in uppercase
-  const name = firstName + lastName;
+  const name = firstName + ' ' + lastName;
   return name.toUpperCase();
 }
 

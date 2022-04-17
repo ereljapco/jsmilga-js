99d39e649c6174007b2a846cdbd7d19368229ae0
Erel Ropeta <helloerelita@gmail.com>
2022-04-17 23:19:19 +0800

    feat(exer-full-name): add the names with the last name and add it to fullNames

--- exer-01-full-name/app.js
+++ exer-01-full-name/app.js
@@ -1,3 +1,7 @@
 const names = ['Percy', 'George', 'Fred', 'Ron', 'Ginny'];
 const lastName = 'Weasley';
 let fullNames = [];
+
+for (let i = 0; i < names.length; i++) {
+  fullNames.push(`${names[i]} ${lastName}`);
+}

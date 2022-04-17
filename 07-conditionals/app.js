834fa954ad41df560995240e7ee5dc792689372e
Erel Ropeta <helloerelita@gmail.com>
2022-04-17 16:04:11 +0800

    feat(conditionals): add name, age, status to the person1 and person2

--- 07-conditionals/app.js
+++ 07-conditionals/app.js
@@ -1,8 +1,14 @@
 // create two objects "person1", "person2"
+// setup name, age (15-25), status ('resident', 'tourist') keys
 const person1 = {
-
+  name: 'Jane Doe',
+  age: 21,
+  status: 'resident'
 }
 
 const person2 = {
-  
+  name: 'John Doe',
+  age: 25,
+  status: 'tourist'
+
 }

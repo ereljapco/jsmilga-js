13e0ccf23ed7b5adabddf248736a0c4198a92129
Erel Ropeta <helloerelita@gmail.com>
2022-04-17 22:27:40 +0800

    feat(strings): concatenate firstName and lastName and return result in uppercase

--- 08-strings/app.js
+++ 08-strings/app.js
@@ -1,5 +1,7 @@
 // create function fullName
 // accept two parameters "firstName" and "lastName"
 function fullName(firstName, lastName) {
-
+  // add "firstName" and "lastName" (concat) and return result in uppercase
+  const name = firstName + lastName;
+  return name.toUpperCase();
 }

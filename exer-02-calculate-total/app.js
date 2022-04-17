52ce2c3c592d78edf0332809014ac12154f263a6
Erel Ropeta <helloerelita@gmail.com>
2022-04-17 23:33:34 +0800

    feat(calculate-total): create function calculateTotal where the sum of the values in the arr will be returned

--- exer-02-calculate-total/app.js
+++ exer-02-calculate-total/app.js
@@ -1,2 +1,12 @@
 const homeBills = [3000, 500, 2299];
 const babyNeeds = [5800, 1600, 4000];
+
+function calculateTotal(arr) {
+  let total = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+
+  return total;
+}

5a5ebcfd23c2a9ffb669d90ab4202537021a0931
Erel Ropeta <helloerelita@gmail.com>
2022-04-18 04:30:23 +0800

    perf(array): move students array from app.js to data.js

---
+++ 09-array/data.js
@@ -0,0 +1,33 @@
+// 1. setup students array with 5 students
+const students = [
+  {
+    id: 1,
+    name: 'Sheldon',
+    score: 100,
+    favoriteSubject: 'Physics'
+  },
+  {
+    id: 2,
+    name: 'Penny',
+    score: 60,
+    favoriteSubject: 'Physical Education'
+  },
+  {
+    id: 3,
+    name: 'Leonard',
+    score: 96,
+    favoriteSubject: 'Physics'
+  },
+  {
+    id: 4,
+    name: 'Howard',
+    score: 86,
+    favoriteSubject: 'Mathematics'
+  },
+  {
+    id: 5,
+    name: 'Raj',
+    score: 85,
+    favoriteSubject: 'Earth Science'
+  }
+];

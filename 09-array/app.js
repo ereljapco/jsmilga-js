b6726e5378e329ce6150edb780f4c5d00b9d22bf
Erel Ropeta <helloerelita@gmail.com>
2022-04-18 23:14:34 +0800

    feat(array): compute the average score of students, assign it to averageScore and log in the console

--- 09-array/app.js
+++ 09-array/app.js
@@ -28,3 +28,14 @@ const specificId = students.find(function(student) {
 });
 
 console.log(specificId);
+
+// reduce - averageScore
+// 1. sum up all student.score values with reduce
+// 2. divide by the length of the students array
+// 3. assign to 'averageScore' and log
+
+const averageScore = students.reduce(function(totalScore, student) {
+  return totalScore + student.score;
+}, 0) / students.length;
+
+console.log(averageScore);

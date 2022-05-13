34a5a9a8f77c4733ee6741ce87dd689430ee5668
Erel Ropeta <helloerelita@gmail.com>
2022-05-14 03:02:43 +0800

    ✨ feat: user can call `deposit` method to deposit and the current balance of the user is log in the console

--- code-along-functions/js/app.js
+++ code-along-functions/js/app.js
@@ -1,11 +1,20 @@
 function newAccount(name, initialBalance) {
   let balance = initialBalance;
+
   function showBalance() {
     console.log(`Hello, ${name}! You're current balance is PHP ${balance}`);
   }
-  return { showBalance: showBalance };
+
+  function deposit(amount) {
+    balance += amount;
+    showBalance();
+  }
+
+  return { showBalance: showBalance, deposit: deposit };
 }
 
 const erel = newAccount('Erel', 3000);
 
 erel.showBalance();
+erel.deposit(500);
+erel.deposit(7230);

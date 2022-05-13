51548ee8796b9a21d8996e18e2177019568a9d81
Erel Ropeta <helloerelita@gmail.com>
2022-05-14 02:53:59 +0800

    ✨ feat: log in the console the current balance of the user account

---
+++ code-along-functions/js/app.js
@@ -0,0 +1,11 @@
+function newAccount(name, initialBalance) {
+  let balance = initialBalance;
+  function showBalance() {
+    console.log(`Hello, ${name}! You're current balance is PHP ${balance}`);
+  }
+  return showBalance;
+}
+
+const erel = newAccount('Erel', 3000);
+
+erel();

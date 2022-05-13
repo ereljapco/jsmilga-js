78b6d87e567a19a5e08f4c7262bf7a393e73227d
Erel Ropeta <helloerelita@gmail.com>
2022-05-14 02:58:41 +0800

    🏅 improve: make showBalance function a method

--- code-along-functions/js/app.js
+++ code-along-functions/js/app.js
@@ -3,9 +3,9 @@ function newAccount(name, initialBalance) {
   function showBalance() {
     console.log(`Hello, ${name}! You're current balance is PHP ${balance}`);
   }
-  return showBalance;
+  return { showBalance: showBalance };
 }
 
 const erel = newAccount('Erel', 3000);
 
-erel();
+erel.showBalance();

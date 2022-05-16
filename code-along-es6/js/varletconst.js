c31b382caf1a7e9d1528b7e52ba04cf60be4cc21
Erel Ropeta <helloerelita@gmail.com>
2022-05-16 17:58:26 +0800

    rename: `app.js` to `varletconst.js`

--- code-along-es6/js/varletconst.js
+++ code-along-es6/js/varletconst.js
@@ -1,43 +1,29 @@
-// var, let, const
-
-// ===== var
-// define var
 var number = 13;
 // console.log(number);
 
-// reassign var
 number = 15;
 // console.log(number);
 
-// redefine var
 var number = `orange`;
 console.log(number);
 
-// ===== let
-// define let
 let word = `word`;
 // console.log(word);
 
-// reassign let
 word = `new`;
 console.log(word);
 
-// redifine let
 // let word = `no`;
 // console.log(word);
 // ERROR: redeclaration
 
-// ===== const
-// define const
 const PHRASE = `define only`;
 console.log(PHRASE);
 
-// reassign const
 // PHRASE = `new phrase`;
 // console.log(PHRASE);
 // ERROR: invalid assignment to const
 
-// redefine const
 // const PHRASE = `can't redefine`;
 // console.log(PHRASE);
 // ERROR: redeclaration

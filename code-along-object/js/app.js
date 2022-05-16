69dcd7e94ff1fbba2c8dff9bb49868ccd48495e1
Erel Ropeta <helloerelita@gmail.com>
2022-05-16 17:29:31 +0800

    refactor(account): log in the console the amount deposietd by the user with the new balance and bank name using `class`

--- code-along-object/js/app.js
+++ code-along-object/js/app.js
@@ -1,44 +1,19 @@
-// class Relationship {
-//   constructor(firstName, lastName, relationship) {
-//     this.firstName = firstName;
-//     this.lastName = lastName;
-//     this.greet = function () {
-//       return `Hello! I'm ${this.firstName} ${this.lastName}, and I'm ${baby.firstName}'s ${relationship}`;
-//     };
-//   }
-// }
-
-// const baby = {
-//   firstName: 'Elyse',
-//   lastName: 'Japco',
-//   parents: {
-//     mother: new Relationship('Erel', 'Japco', 'mother'),
-//     father: new Relationship('Enzo', 'Japco', 'father'),
-//   },
-// };
-
-// console.log(baby.parents.mother.greet());
-// console.log(baby.parents.father.greet());
-
-function Account(name, initialBalance) {
-  this.name = name;
-  this.balance = initialBalance;
+class Account {
+  constructor(name, initialBalance) {
+    this.name = name;
+    this.balance = initialBalance;
+  }
+  bank = 'Unionbank';
+  deposit(amount) {
+    this.balance += amount;
+    console.log(
+      `Hello, ${this.name}! \n\nYou have deposited PHP ${amount}. You're new balance is ${this.balance}.\nThank you for banking with us at ${this.bank}!`
+    );
+  }
 }
 
-Account.prototype.bank = 'Unionbank';
-Account.prototype.deposit = function (amount) {
-  this.balance += amount;
-  console.log(
-    `You have deposited PHP ${amount}. You're new balance is ${this.balance}.\nThank you for banking with us at ${this.bank}!`
-  );
-};
-
 const erel = new Account('Erel Japco', 500);
-
-console.log(erel.bank);
-console.log(erel.balance);
+const enzo = new Account('Enzo Japco', 1000);
 
 erel.deposit(1000);
-erel.deposit(500);
-erel.deposit(12);
-erel.deposit(123);
+enzo.deposit(100);

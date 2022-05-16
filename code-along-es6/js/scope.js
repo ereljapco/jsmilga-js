03f6e50c1d75587115ce32e9daeee6b8995d2f3c
Erel Ropeta <helloerelita@gmail.com>
2022-05-16 19:56:44 +0800

    feat: log the value of `scopeLet` from a function, a block within a function, a block, and global scope

--- code-along-es6/js/scope.js
+++ code-along-es6/js/scope.js
@@ -1,22 +1,34 @@
 const scopeBool = true;
 var scopeVar = `this is the var variable in global`;
+let scopeLet = `this is the let variable in global`;
 
 function hello() {
   scopeVar = `var from function`;
-  console.log(scopeVar);
+  // console.log(scopeVar);
+  scopeLet = `reassigned let from function`;
+  // var scopeLet = `redeclared let from function`;
+  console.log(scopeLet);
+
   if (scopeBool) {
-    // var scopeVar = `var from block within a function`;
-    scopeVar = `var from block within a function`;
-    console.log(scopeVar);
+    var scopeVar = `var from block within a function`;
+    // var scopeLet = `redeclared let from block within a function`;
+    scopeLet = `reassigned let from block within a function`;
+    // console.log(scopeVar);
+    console.log(scopeLet);
   }
 }
 
 hello();
-console.log(scopeVar);
+// console.log(scopeVar);
+console.log(scopeLet);
 
 if (scopeBool) {
-  var scopeVar = `var from block`;
-  console.log(scopeVar);
+  // scopeVar = `var from block`;
+  // let scopeLet = `redeclared let from block`;
+  scopeLet = `reassigned let from block`;
+  // console.log(scopeVar);
+  console.log(scopeLet);
 }
 
-console.log(scopeVar);
+// console.log(scopeVar);
+console.log(scopeLet);

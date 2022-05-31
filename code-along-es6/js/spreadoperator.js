e3a8bdd66210413b36192c3e0a43c3dedbcddc47
Erel Ropeta <helloerelita@gmail.com>
2022-05-31 23:48:39 +0800

    console: log values of `friends`
    - add the values of `boys`, `girls`, and `bestFriend` to `friends` using
    spread operator

--- code-along-es6/js/spreadoperator.js
+++ code-along-es6/js/spreadoperator.js
@@ -1,5 +1,12 @@
 const word = 'picture';
 const letters = [...word];
 
-console.log(word);
-console.log(letters);
+// console.log(word);
+// console.log(letters);
+
+const boys = ['Gary', 'Mr. Krabs', 'Squidward'];
+const girls = ['Pearl', 'Sandy'];
+const bestFriend = 'Patrick';
+
+const friends = [bestFriend, ...boys, ...girls];
+console.log(friends);

5a895b4b811297b96172bcdb12c47aacdc00140c
Erel Ropeta <helloerelita@gmail.com>
2022-05-31 23:13:04 +0800

    console: log (boolean) whether the first 9 characters of `manager` ends with 'MAN', or not

--- code-along-es6/js/newstringmethods.js
+++ code-along-es6/js/newstringmethods.js
@@ -3,4 +3,6 @@ const employee = '23456-EMP-JUAN-DELA-CRUZ';
 const manager = '23456-MAN-JUANA-DELA-CRUZ';
 
 // console.log(person.startsWith('Juan'));
-console.log(employee.startsWith('EMP', 6));
+// console.log(employee.startsWith('EMP', 6));
+
+console.log(manager.endsWith('MAN', 9));

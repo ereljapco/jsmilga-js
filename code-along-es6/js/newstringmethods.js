6982b16eaeb724b3bca5a1f851c0a5eb07763cd1
Erel Ropeta <helloerelita@gmail.com>
2022-05-31 23:08:17 +0800

    console: log whether variable `employee` starts with `EMP` starting at index 6, or not

--- code-along-es6/js/newstringmethods.js
+++ code-along-es6/js/newstringmethods.js
@@ -2,4 +2,5 @@ const person = 'Juan Dela Cruz';
 const employee = '23456-EMP-JUAN-DELA-CRUZ';
 const manager = '23456-MAN-JUANA-DELA-CRUZ';
 
-console.log(person.startsWith('Juan'));
+// console.log(person.startsWith('Juan'));
+console.log(employee.startsWith('EMP', 6));

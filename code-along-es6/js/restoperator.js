b7f0813ee02cef245bf0951a176a3c9aa95ae0ae
Erel Ropeta <helloerelita@gmail.com>
2022-06-01 08:54:02 +0800

    console: log the job of `person`, and its other properties
    - use rest operator to have the other properties as an object

--- code-along-es6/js/restoperator.js
+++ code-along-es6/js/restoperator.js
@@ -2,4 +2,13 @@
 const fruits = ['mango', 'pineapple', 'atis', 'guava', 'starapple'];
 const [first, second, ...otherFruits] = fruits;
 
-console.log(first, second, otherFruits);
+// console.log(first, second, otherFruits);
+
+// ----- OBJECTS
+const person = {
+  firstName: 'Erel',
+  lastName: 'Japco',
+  job: 'developer',
+};
+const { job, ...otherInfo } = person;
+console.log(job, otherInfo);
